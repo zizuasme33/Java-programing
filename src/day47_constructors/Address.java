171cf66a4ea2f2de8bc959eb73b27547532cafae
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-05-27 20:02:29 -0400

    Added address class

---
+++ src/day47_constructors/Address.java
@@ -0,0 +1,56 @@
+package day47_constructors;
+
+public class Address {
+
+    String street;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+
+    }
+
+    @Override
+    public String toString() {
+        return street + ", " + city + ", "+ state + " " + zipCode ;
+    }
+}
