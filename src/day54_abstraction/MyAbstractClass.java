d5877c2faa9bbb42d28a830057794654a2314124
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-06-15 22:06:08 -0400

    Abstract class intro

---
+++ src/day54_abstraction/MyAbstractClass.java
@@ -0,0 +1,29 @@
+package day54_abstraction;
+
+public abstract class MyAbstractClass {
+
+    int num = 55;
+    public void learn() {
+        System.out.println("Learning...");
+    }
+
+    public abstract void close(); // we only need signature in abstract class
+
+}
+
+class Sub extends MyAbstractClass {
+    public void close() {
+        System.out.println("Close - abstract method implementation");
+    }
+}
+
+class MyObjects {
+    public static void main(String[] args) {
+        // MyAbstractClass mac = new MyAbstractClass();
+        Sub sub = new Sub();
+        sub.learn();
+        System.out.println(sub.num);
+        sub.close();
+
+    }
+}
