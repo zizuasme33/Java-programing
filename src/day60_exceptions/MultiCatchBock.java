4eb41874967acac80cc6d6a106568a530056a5e5
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-07-06 23:30:37 -0400

    finally block, throw new example

---
+++ src/day60_exceptions/MultiCatchBock.java
@@ -0,0 +1,25 @@
+package day60_exceptions;
+
+public class MultiCatchBock {
+
+    public static void main(String[] args) {
+        String word = "java";
+        //String word = null;
+        try {
+           // System.out.println(10/0);
+            System.out.println("word = " + word);
+            System.out.println("length = " + word.length());
+            System.out.println(word.substring(20));
+        }catch (NullPointerException e) {
+            System.out.println("NullPointException caught and handles, please check if String has valid value");
+        }catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Index is wrong, please check if you entered a valid index");
+        }catch (Throwable t) { // prent of all exceptions - parent type exception must be at end
+            System.out.println("Exception is caught");
+            System.out.println("Reason = " + t.getMessage());
+        }
+
+        System.out.println("--- more code after try catch ---");
+
+    }
+}
